d248000c2321af6b37012cdfe03cc0e59da4d0ba
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-10-31 21:59:16 -0300

    mudanças nos lides para automaticos

--- index/slider_img.js
+++ index/slider_img.js
@@ -1,14 +1,46 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+const time = 4000;
 
-setInterval(function(){
-    contagem()
-}, 5000);
+let imagemAtual = 0;
+let tituloAtual = 0;
 
-function contagem(){
-    count++;
-    if(count > 4){
-        count = 1;
+const imagens = document.querySelectorAll("#slider img");
+const titulos = document.querySelectorAll("#slider h1");
+
+const imagensMaximas = imagens.length;
+const titulosMaximos = titulos.length;
+
+function proximaImagem(){
+    imagens[imagemAtual].classList.remove("selecionada");
+
+    imagemAtual++
+
+    if(imagemAtual >= imagensMaximas){
+        imagemAtual = 0;
     }
-    document.getElementById("radio" + count).checked = true;
+
+    imagens[imagemAtual].classList.add("selecionada");
 }
+
+function proximoTitulo(){
+
+    titulos[tituloAtual].classList.remove("selecionado");
+
+    tituloAtual++
+
+    if(tituloAtual >= titulosMaximos){
+        tituloAtual = 0;
+    }
+
+    titulos[tituloAtual].classList.add("selecionado");
+}
+
+
+function start(){
+    setInterval(() =>{
+        proximaImagem();
+        proximoTitulo();
+    }, time);
+}
+
+
+window.addEventListener("load", start);
